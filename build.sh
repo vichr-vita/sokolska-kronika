b8cd338c4c6fb00f3cdd7d9d1f0cf5e20afbdef4
Vít Chrubasík <me@vichr.me>
2024-05-24 16:25:48 +0200

    chore: build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+version="1_1_0"
+
+pdflatex -output-directory=out main.tex
+
+
+out_file="./out/sokolska_kronika_v${version}.pdf"
+cp ./out/main.pdf $out_file
+echo "file: ${out_file}"
+
